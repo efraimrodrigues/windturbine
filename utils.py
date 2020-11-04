9ba07ed623f844247fe8ff637101a63f08ced80f
Efraim Rodrigues <efraimnaassom@gmail.com>
2020-11-04 17:19:03 -0300

    Implements global random search.

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import random
 
 def load_data_set():
     x = []
@@ -42,3 +43,43 @@ def r_squared_adj(y, y_hat):
 def aic(y, y_hat):
     ss_res = ((y-y_hat)**2).sum()
     return 2 + 2*np.log(ss_res)
+
+def random_solution(d):
+    a = []
+    for i in range(0, d):
+        if i != d - 1:
+            a.append(random.uniform(-0.00005, 0.00005))
+        else:
+            a.append(0)
+
+    return a
+
+def global_random_search(x, y, d, M, p):
+    best = random_solution(d)
+    f_best = aic(y, np.poly1d(best)(x))
+
+    best_1 = None
+    best_2 = None
+    best_3 = None
+    
+    c = 0
+    for i in range(0, M):
+        candidate = random_solution(d)
+        f_candidate = aic(y, np.poly1d(candidate)(x))
+
+        if  f_candidate < f_best:
+            best = candidate
+            f_best = aic(y, np.poly1d(best)(x))
+
+            
+            best_3 = best_2
+            best_2 = best_1
+            best_1 = best
+
+        else:
+            c = c + 1
+
+        if c == p:
+            break
+            
+    return best_1, best_2, best_3
